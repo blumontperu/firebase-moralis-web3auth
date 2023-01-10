327e911b4d05d0d67a39c1818f9f6d04546a60fc
kidokido <kido2564@gmail.com>
2023-01-10 16:39:38 +0500

    changes apply

--- blumontwallet/src/App.tsx
+++ blumontwallet/src/App.tsx
@@ -7,6 +7,7 @@ import "./App.css";
 import { GoogleAuthProvider, getAuth, signInWithPopup, UserCredential } from "firebase/auth";
 import { Web3AuthCore } from "@web3auth/core";
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from 'firebase/analytics';
 import { useMoralis } from "react-moralis";
 
 const clientId = "BIbOPTHvKSyeu-OnFNp6TID_Aee1LjLO_2WxLXZ3IKa8ud4buuoJZimCRuIMRrzOkFW4Lhrl0uiJ54SxDp3lfHU"; // get from https://dashboard.web3auth.io
@@ -26,7 +27,8 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-function App() {
+
+function  App() {
   const [web3auth, setWeb3auth] = useState<Web3AuthCore | null>(null);
   const [provider, setProvider] = useState<SafeEventEmitterProvider | null>(null);
   const { authenticate, isAuthenticated } = useMoralis();
@@ -42,7 +44,7 @@ function App() {
           chainId: "0x1",
           rpcTarget: "https://rpc.ankr.com/eth",
   },
-        web3AuthNetwork: "testnet",
+        // web3AuthNetwork: "testnet",
 });
 
       const openloginAdapter = new OpenloginAdapter({
@@ -117,153 +119,156 @@ const signInWithGoogle = async (): Promise<UserCredential> => {
         });
     }
   };
-} 
-//
-    const loginRes = await signInWithGoogle();
-    console.log("login details", loginRes);
-    const idToken = await loginRes.user.getIdToken(true); // this idToken will be passed to web3auth
-    console.log("idToken", idToken)
 
-    const web3authProvider = await web3auth.connectTo(
-      WALLET_ADAPTERS.OPENLOGIN,
-      {
-        loginProvider: "jwt",
-        extraLoginOptions: {
-          id_token: idToken,
-          verifierIdField: "sub",
-          domain: "http://localhost:3000",
-        },
-      }
-    );
+//----------------------
+const loginAauth = async () => {
+ const loginRes = await signInWithGoogle();
+console.log("login details", loginRes);
+const idToken = await loginRes.user.getIdToken(true); // this idToken will be passed to web3auth
+console.log("idToken", idToken)
 
-    setProvider(web3authProvider);
-  ;
+const web3authProvider = await web3auth!.connectTo(
+  WALLET_ADAPTERS.OPENLOGIN,
+  {
+    loginProvider: "jwt",
+    extraLoginOptions: {
+      id_token: idToken,
+      verifierIdField: "sub",
+      domain: "http://localhost:3000",
+    },
+  }
+);
 
-  const authenticateUser = async () => {
-		if (!web3auth) {
-			uiConsole('web3auth not initialized yet');
-			return;
-		}
-		const idToken = await web3auth.authenticateUser();
-		uiConsole(idToken);
-	};
+setProvider(web3authProvider);
+
+};
+loginAauth();
+
+const authenticateUser = async () => {
+if (!web3auth) {
+  console.log('web3auth not initialized yet');
+  return;
+}
+const idToken = await web3auth.authenticateUser();
+console.log(idToken);
+};
 
 
-  const getUserInfo = async () => {
-    if (!web3auth) {
-      console.log("web3auth not initialized yet");
-      return;
-    }
-    const user = await web3auth.getUserInfo();
-    console.log(user);
-  };
+const getUserInfo = async () => {
+if (!web3auth) {
+  console.log("web3auth not initialized yet");
+  return;
+}
+const user = await web3auth.getUserInfo();
+console.log(user);
+};
 
-  const logout = async () => {
-    if (!web3auth) {
-      console.log("web3auth not initialized yet");
-      return;
-    }
-    await web3auth.logout();
-    setProvider(null);
-  };
+const logout = async () => {
+if (!web3auth) {
+  console.log("web3auth not initialized yet");
+  return;
+}
+await web3auth.logout();
+setProvider(null);
+};
 
-  const getChainId = async () => {
-    if (!provider) {
-      console.log("provider not initialized yet");
-      return;
-    }
-    const rpc = new RPC(provider);
-    const chainId = await rpc.getChainId();
-    console.log(chainId);
-  };
-  const getAccounts = async () => {
-    if (!provider) {
-      console.log("provider not initialized yet");
-      return;
-    }
-    const rpc = new RPC(provider);
-    const address = await rpc.getAccounts();
-    console.log(address);
-  };
+const getChainId = async () => {
+if (!provider) {
+  console.log("provider not initialized yet");
+  return;
+}
+const rpc = new RPC(provider);
+const chainId = await rpc.getChainId();
+console.log(chainId);
+};
+const getAccounts = async () => {
+if (!provider) {
+  console.log("provider not initialized yet");
+  return;
+}
+const rpc = new RPC(provider);
+const address = await rpc.getAccounts();
+console.log(address);
+};
 
-  const getBalance = async () => {
-    if (!provider) {
-      console.log("provider not initialized yet");
-      return;
-    }
-    const rpc = new RPC(provider);
-    const balance = await rpc.getBalance();
-    console.log(balance);
-  };
+const getBalance = async () => {
+if (!provider) {
+  console.log("provider not initialized yet");
+  return;
+}
+const rpc = new RPC(provider);
+const balance = await rpc.getBalance();
+console.log(balance);
+};
 
-  const sendTransaction = async () => {
-    if (!provider) {
-      console.log("provider not initialized yet");
-      return;
-    }
-    const rpc = new RPC(provider);
-    const receipt = await rpc.sendTransaction();
-    console.log(receipt);
-  };
+const sendTransaction = async () => {
+if (!provider) {
+  console.log("provider not initialized yet");
+  return;
+}
+const rpc = new RPC(provider);
+const receipt = await rpc.sendTransaction();
+console.log(receipt);
+};
 
-  const signMessage = async () => {
-    if (!provider) {
-      console.log("provider not initialized yet");
-      return;
-    }
-    const rpc = new RPC(provider);
-    const signedMessage = await rpc.signMessage();
-    console.log(signedMessage);
-  };
+const signMessage = async () => {
+if (!provider) {
+  console.log("provider not initialized yet");
+  return;
+}
+const rpc = new RPC(provider);
+const signedMessage = await rpc.signMessage();
+console.log(signedMessage);
+};
 
-  const getPrivateKey = async () => {
-    if (!provider) {
-      console.log("provider not initialized yet");
-      return;
-    }
-    const rpc = new RPC(provider);
-    const privateKey = await rpc.getPrivateKey();
-    console.log(privateKey);
-  };
-  const loggedInView = (
-    <>
-      <button onClick={getUserInfo} className="card">
-        Get User Info
-      </button>
-      <button onClick={getChainId} className="card">
-        Get Chain ID
-      </button>
-      <button onClick={getAccounts} className="card">
-        Get Accounts
-      </button>
-      <button onClick={getBalance} className="card">
-        Get Balance
-      </button>
-      <button onClick={sendTransaction} className="card">
-        Send Transaction
-      </button>
-      <button onClick={signMessage} className="card">
-        Sign Message
-      </button>
-      <button onClick={getPrivateKey} className="card">
-        Get Private Key
-      </button>
-      <button onClick={logout} className="card">
-        Log Out
-      </button>
+const getPrivateKey = async () => {
+if (!provider) {
+  console.log("provider not initialized yet");
+  return;
+}
+const rpc = new RPC(provider);
+const privateKey = await rpc.getPrivateKey();
+console.log(privateKey);
+};
+const loggedInView = (
+<>
+  <button onClick={getUserInfo} className="card">
+    Get User Info
+  </button>
+  <button onClick={getChainId} className="card">
+    Get Chain ID
+  </button>
+  <button onClick={getAccounts} className="card">
+    Get Accounts
+  </button>
+  <button onClick={getBalance} className="card">
+    Get Balance
+  </button>
+  <button onClick={sendTransaction} className="card">
+    Send Transaction
+  </button>
+  <button onClick={signMessage} className="card">
+    Sign Message
+  </button>
+  <button onClick={getPrivateKey} className="card">
+    Get Private Key
+  </button>
+  <button onClick={logout} className="card">
+    Log Out
+  </button>
 
-      <div id="console" style={{ whiteSpace: "pre-line" }}>
-        <p style={{ whiteSpace: "pre-line" }}></p>
-      </div>
-    </>
-  );
+  <div id="console" style={{ whiteSpace: "pre-line" }}>
+    <p style={{ whiteSpace: "pre-line" }}></p>
+  </div>
+</>
+);
 
-  const unloggedInView = (
-    <button onClick={login} className="card">
-      Login
-    </button>
-  );
-
+const unloggedInView = (
+<button onClick={login} className="card">
+  Login
+</button>
+);
+  //---------------return---------------
   return (
     <div>
     <div className="container">
@@ -284,6 +289,11 @@ const signInWithGoogle = async (): Promise<UserCredential> => {
     </div>
     </div>
   );
-}
+} 
+//
+  
+
+  
+
 
 export default App;
