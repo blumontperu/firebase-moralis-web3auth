956e59e9dbe2a579935fc41fbb3051cfb6725068
abdul <mateen.fastians@gmail.com>
2023-01-14 21:23:55 +0900

    changes

--- blumontwallet/README.md
+++ blumontwallet/README.md
@@ -1,46 +1,33 @@
-# Getting Started with Create React App
+# Web3Auth x Firebase Demo Application
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## Quick Start
 
-## Available Scripts
+```bash
+git clone https://github.com/Web3Auth/examples.git
+cd web-core-sdk/custom-authentication/firebase-react-core-example
+npm install
+npm run start # localhost:3000
+```
 
-In the project directory, you can run:
+## Configure
 
-### `npm start`
+This guide will cover the basics of how to set up your Web3Auth SDK and Firebase
+account for the integration and provide you with the links on how to develop a
+basic web application on the blockchain of your choice.
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+[Read the full guide to configure.](https://web3auth.io/docs/guides/firebase)
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+## Questions
 
-### `npm test`
+Ask us on our
+[Github Discussion Board](https://github.com/orgs/Web3Auth/discussions) or reach
+out to our [Discord](https://discord.gg/web3auth).
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+## Important Links
 
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
+- [Website](https://web3auth.io)
+- [Docs](https://web3auth.io/docs)
+- [Guides](https://web3auth.io/docs/guides)
+- [SDK / API References](https://web3auth.io/docs/sdk)
+- [Pricing](https://web3auth.io/pricing.html)
+- [Github Discussion Board](https://github.com/orgs/Web3Auth/discussions)
--- blumontwallet/src/App.tsx
+++ blumontwallet/src/App.tsx
@@ -1,90 +1,65 @@
 import { useEffect, useState } from "react";
-// HIGHLIGHTSTART-importModules //
+import { Web3AuthCore } from "@web3auth/core";
 import {
   WALLET_ADAPTERS,
   CHAIN_NAMESPACES,
   SafeEventEmitterProvider,
 } from "@web3auth/base";
 import { OpenloginAdapter } from "@web3auth/openlogin-adapter";
-import RPC from "./web3RPC";
 import "./App.css";
+// import RPC from "./evm.web3";
+import RPC from "./evm.ethers";
+import { initializeApp } from "firebase/app";
 import {
   GoogleAuthProvider,
   getAuth,
   signInWithPopup,
   UserCredential,
 } from "firebase/auth";
-import { Web3AuthCore } from "@web3auth/core";
-import { initializeApp } from "firebase/app";
-import { useMoralis } from "react-moralis";
-import { getAnalytics } from 'firebase/analytics';
 
 const clientId =
-  "BIbOPTHvKSyeu-OnFNp6TID_Aee1LjLO_2WxLXZ3IKa8ud4buuoJZimCRuIMRrzOkFW4Lhrl0uiJ54SxDp3lfHU"; // get from https://dashboard.web3auth.io
+  "BEglQSgt4cUWcj6SKRdu5QkOXTsePmMcusG5EAoyjyOYKlVRjIF1iCNnMOTfpzCiunHRrMui8TIwQPXdkQ8Yxuk"; // get from https://dashboard.web3auth.io
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBpBD8bmvyE9aQJ88t5viyJ3JuNzqTT2jc",
-  authDomain: "blumontapp.firebaseapp.com",
-  databaseURL: "https://blumontapp-default-rtdb.firebaseio.com",
-  projectId: "blumontapp",
-  storageBucket: "blumontapp.appspot.com",
-  messagingSenderId: "208401884324",
-  appId: "1:208401884324:web:d071a338a4622b0b2cf231",
-  measurementId: "G-TXNF3LGM8F",
+  apiKey: "AIzaSyB0nd9YsPLu-tpdCrsXn8wgsWVAiYEpQ_E",
+  authDomain: "web3auth-oauth-logins.firebaseapp.com",
+  projectId: "web3auth-oauth-logins",
+  storageBucket: "web3auth-oauth-logins.appspot.com",
+  messagingSenderId: "461819774167",
+  appId: "1:461819774167:web:e74addfb6cc88f3b5b9c92",
 };
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-async function App() {
+function App() {
   const [web3auth, setWeb3auth] = useState<Web3AuthCore | null>(null);
   const [provider, setProvider] = useState<SafeEventEmitterProvider | null>(
     null
   );
-  const { authenticate, isAuthenticated } = useMoralis();
 
   useEffect(() => {
     const init = async () => {
       try {
         const web3auth = new Web3AuthCore({
-          clientId:
-            "BIbOPTHvKSyeu-OnFNp6TID_Aee1LjLO_2WxLXZ3IKa8ud4buuoJZimCRuIMRrzOkFW4Lhrl0uiJ54SxDp3lfHU",
+          clientId,
           chainConfig: {
-            chainNamespace: CHAIN_NAMESPACES.EIP155, // SOLANA, OTHER
-            chainId: "0x1",
-            rpcTarget: "https://rpc.ankr.com/eth",
+            chainNamespace: CHAIN_NAMESPACES.EIP155,
+            chainId: "0x5",
           },
-          web3AuthNetwork: "testnet",
+          web3AuthNetwork: "cyan"
         });
 
         const openloginAdapter = new OpenloginAdapter({
           adapterSettings: {
-            network: "testnet",
-            clientId:
-              "BIbOPTHvKSyeu-OnFNp6TID_Aee1LjLO_2WxLXZ3IKa8ud4buuoJZimCRuIMRrzOkFW4Lhrl0uiJ54SxDp3lfHU", //Optional - Provide only if you haven't provided it in the Web3Auth Instantiation Code
             uxMode: "redirect",
-            whiteLabel: {
-              name: "BlumontWallet",
-              logoLight: "https://web3auth.io/images/w3a-L-Favicon-1.svg",
-              logoDark: "./public/blumontfavicon.png",
-              defaultLanguage: "es",
-              dark: true, // whether to enable dark mode. defaultValue: false
-            },
-            // HIGHLIGHTSTART-customAuthenticationStep
             loginConfig: {
-              // Add login configs corresponding to the providers on modal. Add other login providers here
               jwt: {
-                name: "blumontapp",
-                verifier: "firebase-blumontapp", // Please create a verifier on the developer dashboard and pass the name here
-                typeOfLogin: "jwt", // Pass on the login provider of the verifier you've created
-                clientId:
-                  "BIbOPTHvKSyeu-OnFNp6TID_Aee1LjLO_2WxLXZ3IKa8ud4buuoJZimCRuIMRrzOkFW4Lhrl0uiJ54SxDp3lfHU", // Pass on the clientId of the login provider here - Please note this differs from the Web3Auth ClientID. This is the JWT Client ID
+                verifier: "web3auth-firebase-examples",
+                typeOfLogin: "jwt",
+                clientId,
               },
             },
           },
         });
-
         web3auth.configureAdapter(openloginAdapter);
         setWeb3auth(web3auth);
 
@@ -106,6 +81,7 @@ async function App() {
       const auth = getAuth(app);
       const googleProvider = new GoogleAuthProvider();
       const res = await signInWithPopup(auth, googleProvider);
+      console.log(res);
       return res;
     } catch (err) {
       console.error(err);
@@ -115,188 +91,168 @@ async function App() {
 
   const login = async () => {
     if (!web3auth) {
-      console.log("web3auth not initialized yet");
+      uiConsole("web3auth not initialized yet");
       return;
     }
-    //
-    if (!isAuthenticated) {
-      await authenticate({
-        provider: "web3Auth",
-        clientId:
-          "BIbOPTHvKSyeu-OnFNp6TID_Aee1LjLO_2WxLXZ3IKa8ud4buuoJZimCRuIMRrzOkFW4Lhrl0uiJ54SxDp3lfHU",
-      })
-        .then(function (user) {
-          console.log(user!.get("ethAddress"));
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    }
+    const loginRes = await signInWithGoogle();
+    console.log("login details", loginRes);
+    const idToken = await loginRes.user.getIdToken(true);
+    console.log("idToken", idToken);
+
+    const web3authProvider = await web3auth.connectTo(
+      WALLET_ADAPTERS.OPENLOGIN,
+      {
+        loginProvider: "jwt",
+        extraLoginOptions: {
+          id_token: idToken,
+          verifierIdField: "sub",
+          domain: "http://localhost:3000",
+        },
+      }
+    );
+    setProvider(web3authProvider);
   };
 
-  //
-  const loginRes = await signInWithGoogle();
-  console.log("login details", loginRes);
-  const idToken = await loginRes.user.getIdToken(true); // this idToken will be passed to web3auth
-  console.log("idToken", idToken);
+  const authenticateUser = async () => {
+		if (!web3auth) {
+			uiConsole('web3auth not initialized yet');
+			return;
+		}
+		const idToken = await web3auth.authenticateUser();
+		uiConsole(idToken);
+	};
 
-  const web3authProvider = await web3auth.connectTo(WALLET_ADAPTERS.OPENLOGIN, {
-    loginProvider: "jwt",
-    extraLoginOptions: {
-      id_token: idToken,
-      verifierIdField: "sub",
-      domain: "http://localhost:3000",
-    },
-  });
-
-  setProvider(web3authProvider);
-  const g = async () => {
+  const getUserInfo = async () => {
     if (!web3auth) {
       uiConsole("web3auth not initialized yet");
       return;
     }
-    const idToken = await web3auth.authenticateUser();
-    uiConsole(idToken);
-  };
-
-  const getUserInfo = async () => {
-    if (!web3auth) {
-      console.log("web3auth not initialized yet");
-      return;
-    }
     const user = await web3auth.getUserInfo();
-    console.log(user);
+    uiConsole(user);
   };
 
   const logout = async () => {
     if (!web3auth) {
-      console.log("web3auth not initialized yet");
+      uiConsole("web3auth not initialized yet");
       return;
     }
     await web3auth.logout();
     setProvider(null);
   };
 
-  const getChainId = async () => {
-    if (!provider) {
-      console.log("provider not initialized yet");
-      return;
-    }
-    const rpc = new RPC(provider);
-    const chainId = await rpc.getChainId();
-    console.log(chainId);
-  };
   const getAccounts = async () => {
     if (!provider) {
-      console.log("provider not initialized yet");
+      uiConsole("provider not initialized yet");
       return;
     }
     const rpc = new RPC(provider);
-    const address = await rpc.getAccounts();
-    console.log(address);
+    const userAccount = await rpc.getAccounts();
+    uiConsole(userAccount);
   };
 
   const getBalance = async () => {
     if (!provider) {
-      console.log("provider not initialized yet");
+      uiConsole("provider not initialized yet");
       return;
     }
     const rpc = new RPC(provider);
     const balance = await rpc.getBalance();
-    console.log(balance);
-  };
-
-  const sendTransaction = async () => {
-    if (!provider) {
-      console.log("provider not initialized yet");
-      return;
-    }
-    const rpc = new RPC(provider);
-    const receipt = await rpc.sendTransaction();
-    console.log(receipt);
+    uiConsole(balance);
   };
 
   const signMessage = async () => {
     if (!provider) {
-      console.log("provider not initialized yet");
+      uiConsole("provider not initialized yet");
       return;
     }
     const rpc = new RPC(provider);
-    const signedMessage = await rpc.signMessage();
-    console.log(signedMessage);
+    const result = await rpc.signMessage();
+    uiConsole(result);
   };
 
-  const getPrivateKey = async () => {
+  const sendTransaction = async () => {
     if (!provider) {
-      console.log("provider not initialized yet");
+      uiConsole("provider not initialized yet");
       return;
     }
     const rpc = new RPC(provider);
-    const privateKey = await rpc.getPrivateKey();
-    console.log(privateKey);
+    const result = await rpc.signAndSendTransaction();
+    uiConsole(result);
   };
-  const loggedInView = (
+
+  function uiConsole(...args: any[]): void {
+    const el = document.querySelector("#console>p");
+    if (el) {
+      el.innerHTML = JSON.stringify(args || {}, null, 2);
+    }
+  }
+
+  const loginView = (
     <>
-      <button onClick={getUserInfo} className="card">
-        Get User Info
-      </button>
-      <button onClick={getChainId} className="card">
-        Get Chain ID
-      </button>
-      <button onClick={getAccounts} className="card">
-        Get Accounts
-      </button>
-      <button onClick={getBalance} className="card">
-        Get Balance
-      </button>
-      <button onClick={sendTransaction} className="card">
-        Send Transaction
-      </button>
-      <button onClick={signMessage} className="card">
-        Sign Message
-      </button>
-      <button onClick={getPrivateKey} className="card">
-        Get Private Key
-      </button>
-      <button onClick={logout} className="card">
-        Log Out
-      </button>
+      <div className="flex-container">
+        <div>
+          <button onClick={getUserInfo} className="card">
+            Get User Info
+          </button>
+        </div>
+        <div>
+					<button onClick={authenticateUser} className='card'>
+						Get ID Token
+					</button>
+				</div>
+        <div>
+          <button onClick={getAccounts} className="card">
+            Get Accounts
+          </button>
+        </div>
+        <div>
+          <button onClick={getBalance} className="card">
+            Get Balance
+          </button>
+        </div>
+        <div>
+          <button onClick={signMessage} className="card">
+            Sign Message
+          </button>
+        </div>
+        <div>
+          <button onClick={sendTransaction} className="card">
+            Send Transaction
+          </button>
+        </div>
+        <div>
+          <button onClick={logout} className="card">
+            Log Out
+          </button>
+        </div>
+      </div>
 
       <div id="console" style={{ whiteSpace: "pre-line" }}>
-        <p style={{ whiteSpace: "pre-line" }}></p>
+        <p style={{ whiteSpace: "pre-line" }}>Logged in Successfully!</p>
       </div>
     </>
   );
 
-  const unloggedInView = (
+  const logoutView = (
     <button onClick={login} className="card">
       Login
     </button>
   );
 
   return (
-    <div>
-      <div className="container">
-        <h1 className="title">
-          <a target="_blank" href="http://web3auth.io/" rel="noreferrer">
-            Web3Auth
-          </a>
-          & Firebase & Moralis & Web3Auth Demo
-        </h1>
+    <div className="container">
+      <h1 className="title">
+        <a target="_blank" href="http://web3auth.io/" rel="noreferrer">
+          Web3Auth
+        </a>{" "}
+        & Firebase React Example for Google Login
+      </h1>
 
-        <div className="grid">{provider ? loggedInView : unloggedInView}</div>
+      <div className="grid">{provider ? loginView : logoutView}</div>
 
-        <footer className="footer">
-          <a
-            href="https://github.com/Web3Auth/Web3Auth/tree/master/examples/react-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Source code
-          </a>
-        </footer>
-      </div>
+      
     </div>
   );
 }
+
 export default App;
